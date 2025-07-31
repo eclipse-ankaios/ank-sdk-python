76a9f15d7c5f0625242aa842823dee74c55ca028
krucod3 <36224699+krucod3@users.noreply.github.com>
2025-07-31 16:54:34 +0200

    Bump version to 0.6.0

--- ankaios_sdk/utils.py
+++ ankaios_sdk/utils.py
@@ -40,7 +40,7 @@ from ._protos import _ank_base
 SUPPORTED_API_VERSION = "v0.1"
 "(str): The supported API version of the Ankaios SDK."
 
-ANKAIOS_VERSION = "0.6.0-rc1"
+ANKAIOS_VERSION = "0.6.0"
 "(str): The version of the compatible Ankaios."
 
 WORKLOADS_PREFIX = "desiredState.workloads"
